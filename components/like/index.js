28a3e63da2abc89e004a48f00ea600e1e454b732
fangweiren <fwr843@163.com>
2020-02-25 16:03:07 +0800

    第6章 组件数据、事件与属性

--- components/like/index.js
+++ components/like/index.js
@@ -1,25 +1,38 @@
 // components/like/index.js
 Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        like: {
+            type: Boolean
+        },
+        count: {
+            type: Number
+        }
+    },
 
-  },
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        yesSrc: "images/like.png",
+        noSrc: "images/like@dis.png"
+    },
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        onLike: function (event) {
+            let like = this.properties.like;
+            let count = this.properties.count;
 
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-    onLike:function (event) {
-      console.log(event);
+            count = like ? count - 1 : count + 1;
+            this.setData({
+                count: count,
+                like: !like
+            })
+        }
     }
-  }
 })
